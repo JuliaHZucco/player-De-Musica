def9369e588ac200ec8339518d614cfc11915426
Júlia H. Zucco <juliahzucco@gmail.com>
2025-10-03 10:24:28 -0300

    Finalização da aplicação. Botão de like implementado

--- script.js
+++ script.js
@@ -11,29 +11,34 @@ const shuffleButton = document.getElementById("shuffle");
 const repeatButton = document.getElementById("repeat");
 const songTime = document.getElementById("song-time");
 const totalTime = document.getElementById("total-time");
+const likeButton = document.getElementById("like");
 
 const millionReasons = {
     songName : "Million Reasons",
     artist : "Lady Gaga",
-    file : "million_reasons"
+    file : "million_reasons",
+    liked: false
 }
 
 const badRomance = {
     songName : "Bad Romance",
     artist : "Lady Gaga",
-    file : "bad_romance"
+    file : "bad_romance",
+    liked: false
 }
 
 const justDance = {
     songName : "Just Dance",
     artist : "Lady Gaga",
-    file : "just_dance"
+    file : "just_dance",
+    liked: false
 }
 
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [millionReasons, badRomance, justDance];
+
+const originalPlaylist = JSON.parse(localStorage.getItem("playlist")) ?? [millionReasons, badRomance, justDance];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
@@ -59,11 +64,24 @@ function playPauseDecider(){
     }
 }
 
+function likeButtonRender(){
+    if (sortedPlaylist[index].liked === true){
+        likeButton.querySelector(".bi").classList.remove("bi-heart");
+        likeButton.querySelector(".bi").classList.add("bi-heart-fill");
+        likeButton.classList.add("button-active");
+    } else {
+        likeButton.querySelector(".bi").classList.add("bi-heart");
+        likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
+        likeButton.classList.remove("button-active");
+    }
+}
+
 function initializeSong(){
     cover.src = `imagens/${sortedPlaylist[index].file}.jpg`;
     song.src = `songs/${sortedPlaylist[index].file}.mp3`;
     songName.innerText = sortedPlaylist[index].songName;
     bandName.innerText = sortedPlaylist[index].artist;
+    likeButtonRender();
 }
 
 function previousSong(){
@@ -169,6 +187,16 @@ function updateTotalTime(){
     
 }
 
+function likeButtonClicked(){
+    if (sortedPlaylist[index].liked === false){
+        sortedPlaylist[index].liked = true;
+    } else {
+        sortedPlaylist[index].liked = false;
+    } 
+    likeButtonRender(); 
+    localStorage.setItem("playlist", JSON.stringify(originalPlaylist)); 
+}
+
 initializeSong();
 
 play.addEventListener("click", playPauseDecider);
@@ -180,3 +208,4 @@ song.addEventListener("loadedmetadata", updateTotalTime);
 progressContainer.addEventListener("click", jumpTo);
 shuffleButton.addEventListener("click", shuffleButtonClicked);
 repeatButton.addEventListener("click", repeatButtonClicked);
+likeButton.addEventListener("click", likeButtonClicked);
